282e075c7348e908748fac342fe7d86013c9b9a5
Raj <rajshah@outlook.ie>
2020-04-20 00:39:15 +0530

    prepare output video

---
+++ prepare_output_videos.py
@@ -0,0 +1,24 @@
+import os
+import glob
+import cv2
+import frame_processing
+
+BASE_FOLDER = 'test_videos'
+VIZ_FOLDER = 'viz_predictions'
+RESULTS_FOLDER = os.path.join(BASE_FOLDER, VIZ_FOLDER)
+
+video_list = glob.glob('test_videos/viz_predictions/*')
+
+total_videos = len(video_list)
+
+for idx, video in enumerate(video_list):
+    print('Processing [{:02d}/{:02d}]: {}'.format((idx + 1), total_videos, video))
+
+    vidcap = cv2.VideoCapture(os.path.join('../test_videos/covid19', '{}.mp4'.format(video)))
+    FRAME_RATE = vidcap.get(cv2.CAP_PROP_FPS)
+    vidcap.release()
+
+    OUTPUT_FRAME_FOLDER = os.path.join(RESULTS_FOLDER, video, 'output_frames')
+    OUTPUT_VIDEO = os.path.join(RESULTS_FOLDER, video, '{}.mp4'.format(video))
+
+    frame_processing.frames_to_video(OUTPUT_FRAME_FOLDER, OUTPUT_VIDEO, FRAME_RATE)
