ba677f9ba8913b87d42024f427c01f453e49a640
Raj <rajshah@outlook.ie>
2020-04-20 00:53:52 +0530

    file path error 3

--- prepare_output_videos.py
+++ prepare_output_videos.py
@@ -17,9 +17,9 @@ for idx, video in enumerate(video_list):
     FRAME_RATE = vidcap.get(cv2.CAP_PROP_FPS)
     vidcap.release()
 
-    print('Processing [{:02d}/{:02d}]: {} | FPS: {}'.format((idx + 1), total_videos, video, FRAME_RATE))
-
     OUTPUT_FRAME_FOLDER = os.path.join(RESULTS_FOLDER, VIDEO_NAME, 'output_frames')
+    print('Processing [{:02d}/{:02d}]: {} | FPS: {}'.format((idx + 1), total_videos, video, FRAME_RATE))
+    print('\toutput frames: {}'.format(OUTPUT_FRAME_FOLDER))
     OUTPUT_VIDEO = os.path.join(RESULTS_FOLDER, VIDEO_NAME, '{}.mp4'.format(VIDEO_NAME))
 
     frame_processing.frames_to_video(OUTPUT_FRAME_FOLDER, OUTPUT_VIDEO, FRAME_RATE)
