6d4196f2cff84fa0ada62c8d19c14f241659b199
Raj <rajshah@outlook.ie>
2020-04-20 11:43:28 +0530

    per video fps

--- frame_processing.py
+++ frame_processing.py
@@ -1,9 +1,5 @@
 import os
 import cv2
-import glob
-
-# requires system packages libx264-dev, libx265-dev, x264, x265
-OPENCV_PY_MP4_CODEC = 0x7634706d
 
 def video_to_frames(input_video, output_folder):
     '''
@@ -21,46 +17,23 @@ def video_to_frames(input_video, output_folder):
     # Get frame rate
     FRAME_RATE = int(vidcap.get(cv2.CAP_PROP_FPS))
 
-    count = 0
-    success = True
-    while success:
-        try:
-            # Read frame and get the shape
-            success, image = vidcap.read()
-
-            if image is not None:
-                # Write the image
-                cv2.imwrite(os.path.join(output_folder, "{}.jpg".format(count)), image)
-
-            # Increment the count
-            count = count + 1
-
-        except Exception as exc:
-            print('error extracting frame {}. error: {}'.format(count, exc))
+    # count = 0
+    # success = True
+    # while success:
+    #     try:
+    #         # Read frame and get the shape
+    #         success, image = vidcap.read()
+    #
+    #         if image is not None:
+    #             # Write the image
+    #             cv2.imwrite(os.path.join(output_folder, "{}.jpg".format(count)), image)
+    #
+    #         # Increment the count
+    #         count = count + 1
+    #
+    #     except Exception as exc:
+    #         print('error extracting frame {}. error: {}'.format(count, exc))
 
     vidcap.release()
 
     return FRAME_RATE
-
-
-def frames_to_video(frames_folder, output_video_path, frame_rate):
-
-    frame_list = glob.glob(os.path.join(frames_folder, '*'))
-
-    img = cv2.imread(frame_list[0])
-    height, width, layers = img.shape
-    size = (width, height)
-
-    out = cv2.VideoWriter(
-        output_video_path, OPENCV_PY_MP4_CODEC, frame_rate, size)
-
-    for img_name in frame_list:
-        try:
-            img = cv2.imread(img_name)
-            out.write(img)
-
-        except Exception as exc:
-            print('error stitching. error {}'.format(exc))
-            break
-
-    out.release()
---
+++ prepare_output_video.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import cv2
+import argparse
+
+# requires system packages libx264-dev, libx265-dev, x264, x265
+OPENCV_PY_MP4_CODEC = 0x7634706d
+
+def frames_to_video(frames_folder, output_video_path, frame_rate):
+
+    frame_list = glob.glob(os.path.join(frames_folder, '*'))
+
+    img = cv2.imread(frame_list[0])
+    height, width, layers = img.shape
+    size = (width, height)
+
+    out = cv2.VideoWriter(
+        output_video_path, OPENCV_PY_MP4_CODEC, frame_rate, size)
+
+    for img_name in frame_list:
+        try:
+            img = cv2.imread(img_name)
+            out.write(img)
+
+        except Exception as exc:
+            print('error stitching. error {}'.format(exc))
+            break
+
+    out.release()
+
+RESULTS_FOLDER = 'viz_predictions'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--out_frames', type=str, help='output frames')
+parser.add_argument('--frame_rate', type=str, help='frame rate')
+args = parser.parse_args()
+
+VIDEO_NAME = args.out_frames.split('/')[-2]
+VIDEO_PATH = os.path.join(RESULTS_FOLDER, VIDEO_NAME, '{}.mp4'.format(VIDEO_NAME))
+
+print('Processing {}'.format(VIDEO_NAME))
+print('\toutput video: {}'.format(VIDEO_PATH))
+
+frames_to_video(args.out_frames, VIDEO_PATH, args.frame_rate)
